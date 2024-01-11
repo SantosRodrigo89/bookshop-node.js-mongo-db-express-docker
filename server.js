8326e6b30865149e813636a9d698e765b3fc76be
SantosRodrigo89 <rodrigo.v.santios89@gmail.com>
2024-01-10 22:42:27 -0300

    feat: add nodemon

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ import http from "http";
 
 const PORT = 3000
 
+const rotas = {
+    "/": "Curso de Express API",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-type": "text/plain" });
-    res.end("Curso de node js")
+    res.end(rotas[req.url])
 })
 
 server.listen(PORT, () => {
