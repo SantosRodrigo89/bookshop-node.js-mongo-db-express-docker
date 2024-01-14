4d3eceb593a1ff613f94abcba8ee281bcd7a3ee9
SantosRodrigo89 <rodrigo.v.santios89@gmail.com>
2024-01-14 03:00:27 -0300

    unindo livros e autores

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -3,10 +3,10 @@ import AutorController from "../controllers/autorController.js";
 
 const routes = express.Router();
 
-routes.get("/livros", AutorController.listarAutores);
-routes.get("/livros/:id", AutorController.listarAutorPorId);
-routes.post("/livros/", AutorController.cadastrarAutor);
-routes.put("/livros/:id", AutorController.atualizarAutor);
-routes.delete("/livros/:id", AutorController.excluirAutor);
+routes.get("/autores", AutorController.listarAutores);
+routes.get("/autores/:id", AutorController.listarAutorPorId);
+routes.post("/autores", AutorController.cadastrarAutor);
+routes.put("/autores/:id", AutorController.atualizarAutor);
+routes.delete("/autores/:id", AutorController.excluirAutor);
 
 export default routes;
