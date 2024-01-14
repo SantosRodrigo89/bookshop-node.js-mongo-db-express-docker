e37d19a015463918b0be30a39000b91f59a572e3
SantosRodrigo89 <rodrigo.v.santios89@gmail.com>
2024-01-14 14:43:19 -0300

    feat: validando erros

--- src/middlewars/manipuladorDeErros.js
+++ src/middlewars/manipuladorDeErros.js
@@ -7,10 +7,19 @@ function manipuladorDeErros(error, req, res, next) {
       .status(400)
       .send({ message: 'Um ou mais dados específicos estão incorretos' });
   }
-  
-  res
-    .status(500)
-    .json({ message: `${error.message} - falha na requisição` });
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    const messageError = Object.values(error.errors)
+      .map((erro) => erro.message)
+      .join('; ');
+    res
+      .status(400)
+      .send({
+        message: `Os seguintes erros foram encontrados: ${messageError}`,
+      });
+  }
+
+  res.status(500).json({ message: `${error.message} - falha na requisição` });
 }
 
 export default manipuladorDeErros;
