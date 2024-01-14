6719202a33510057cfc899619dfc0f9fe5734525
SantosRodrigo89 <rodrigo.v.santios89@gmail.com>
2024-01-14 20:21:10 -0300

    feat: add validations

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -18,8 +18,13 @@ const livroSchema = new mongoose.Schema(
     preco: { type: Number },
     paginas: {
       type: Number,
-      min: [10, 'O número de páginas deve estar entre 10 e 5000. Valor Fornecido {VALUE}'],
-      max: [5000, 'O número de páginas deve estar entre 10 e 5000.  Valor Fornecido {VALUE}'],
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message:
+          'O número de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}',
+      },
     },
     autor: autorSchema,
   },
